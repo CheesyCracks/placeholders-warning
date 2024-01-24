003cad8ebd80bc2abcaac8485ec2267e7e912b6f
CheesyCracks <CheesyCracks@proton.me>
2024-01-24 06:33:53 -0500

    The rest of the plugin

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# Example
-An example greeter plugin
+# Placeholders Warning
+alerts you when the "Always Set Placeholders" button in your bank is disabled.
+## description
+### If you're like me, you've grown tired of coming back to your bank, only to realise that in ages past, through the folly of man, you forgot to turn that one button back on that separates order from chaos and distinguishes man from beast.
+### This plugin will do it's best to steer you towards redemption any time this core pillar of civilization is turned off, before your hubris bears torment to your soul as you flounder through the ruins of your once kempt bank.
+## configuration
+### By default, this plugin will flicker in your eyes (blink the icon), whisper in your ear (send a game message), and echo in your mind (send a notification) every time you are in danger. If you don't like what it's doing, you can persuade it to stop.
---
+++ src/main/java/com/placeholderswarning/PlaceholdersWarningConfig.java
@@ -0,0 +1,40 @@
+package com.placeholderswarning;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+import net.runelite.client.config.ConfigItem;
+
+@ConfigGroup("placeholderswarning")
+public interface PlaceholdersWarningConfig extends Config
+{
+	@ConfigItem(
+			keyName = "blink",
+			name = "Icon Blinking",
+			description = "Blink the button's icon every tick",
+			position = 1
+	)
+	default boolean blink()
+	{
+		return true;
+	}
+	@ConfigItem(
+			keyName = "chatmessage",
+			name = "Chat Message",
+			description = "Receive a game message in chat when opening the bank",
+			position = 2
+	)
+	default boolean chatmessage()
+	{
+		return true;
+	}
+	@ConfigItem(
+			keyName = "notification",
+			name = "Notification",
+			description = "Receive a notification when opening the bank",
+			position = 3
+	)
+	default boolean notification()
+	{
+		return true;
+	}
+}
