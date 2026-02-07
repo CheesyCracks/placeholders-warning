2bcb3fc02b9679eaa659a77ea1f5f9373b50454c
CheesyCracks <111711069+CheesyCracks@users.noreply.github.com>
2026-02-07 15:48:43 -0500

    1.2 - fix for new game version

--- src/main/java/com/placeholderswarning/PlaceholdersWarningPlugin.java
+++ src/main/java/com/placeholderswarning/PlaceholdersWarningPlugin.java
@@ -7,7 +7,7 @@ import net.runelite.api.ChatMessageType;
 import net.runelite.api.Client;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.WidgetLoaded;
-import net.runelite.api.widgets.InterfaceID;
+import net.runelite.api.gameval.InterfaceID;
 import net.runelite.api.widgets.Widget;
 import net.runelite.client.callback.ClientThread;
 import net.runelite.client.config.ConfigManager;
@@ -40,23 +40,23 @@ public class PlaceholdersWarningPlugin extends Plugin
 	public void onConfigChanged(ConfigChanged configChanged) {
 		if (!configChanged.getGroup().equals("placeholderswarning")) return;
 		if (!configChanged.getKey().equals("blink")) return;
-		Widget button = client.getWidget(12, 40);
-		clientThread.invokeLater(() ->{if (button != null && !button.isHidden()) Objects.requireNonNull(client.getWidget(12, 39)).setHidden(false);});
+		Widget button = client.getWidget(InterfaceID.Bankmain.PLACEHOLDER);
+		clientThread.invokeLater(() ->{if (button != null && !button.isHidden()) Objects.requireNonNull(client.getWidget(InterfaceID.Bankmain.PLACEHOLDER_GRAPHIC)).setHidden(false);});
 	}
 	@Subscribe
 	public void onGameTick(GameTick gameTick){
 		if (!config.blink()) return;
-		Widget button = client.getWidget(12, 40);
+		Widget button = client.getWidget(InterfaceID.Bankmain.PLACEHOLDER);
 		if (button == null || button.isHidden()) return;
-		Widget icon = client.getWidget(12, 41);
+		Widget icon = client.getWidget(InterfaceID.Bankmain.PLACEHOLDER_GRAPHIC);
 		assert icon != null;
 		if (button.getSpriteId() != 170) icon.setHidden(false);
 		else icon.setHidden(!icon.isHidden());
 	}
 	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded widgetLoaded) {
-		if (widgetLoaded.getGroupId() != InterfaceID.BANK) return;
-		if (Objects.requireNonNull(client.getWidget(12, 40)).getSpriteId() != 170) return;
+		if (widgetLoaded.getGroupId() != InterfaceID.BANKMAIN) return;
+		if (Objects.requireNonNull(client.getWidget(InterfaceID.Bankmain.PLACEHOLDER)).getSpriteId() != 170) return;
 		if (config.notification().isEnabled()) notifier.notify(config.notification(), "Always Set Placeholders is turned off!");
 		if (config.chatmessage()) client.addChatMessage(ChatMessageType.GAMEMESSAGE, "", "<col=ff0000>Always Set Placeholders is turned off!</col>", null);
 	}
